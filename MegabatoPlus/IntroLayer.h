128f8657a8a9e262f57f76992ebd6447f82c2651
Luis <giga.cacho@gmail.com>
2012-10-10 12:12:45 -0500

    Initial Commit

---
+++ MegabatoPlus/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  MegabatoPlus
+//
+//  Created by Luis Venegas on 10/10/12.
+//  Copyright Luis Venegas 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
